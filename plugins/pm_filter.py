a39e1380be6434280c056e4bbb1f1b19fae7bfda
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2023-10-25 18:47:10 +0530

    ᴊᴏʜɴ ᴡɪᴄᴋ ❗

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -426,7 +426,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('ɢʀᴏᴜᴩ 👥', url='https://t.me/zb_movies'),
+            InlineKeyboardButton('ɢʀᴏᴜᴩ 👥', url='https://t.me/+TB2kIj9B8eIzOTk1'),
             InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ 📮', url='https://t.me/zb_movies_official')
         ], [
             InlineKeyboardButton('⚠️ ʜᴇʟᴘ', callback_data='help'),
